6c7f84275e20cf0e3db873e491d4047e2e0d48c5
Stepan Karpenko <stepan.karpenko@gmail.com>
2019-03-03 21:10:39 -0800

    Added Ultiparc module and environment configuration script.

---
+++ setenv.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# The UltiSoC Project.
+# Environment configuration.
+
+echo ""
+echo "Setting environment for UltiSoC."
+echo "================================="
+echo ""
+
+# Set work directory
+export ULTISOC_HOME=`pwd`
+echo "Workspace: $ULTISOC_HOME"
+
+echo ""
