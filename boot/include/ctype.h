336a28a66390684684f1a0c41fecca5ce90f70d7
Stepan Karpenko <stepan.karpenko@gmail.com>
2019-03-24 18:07:04 -0700

    ctype support.

---
+++ boot/include/ctype.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018-2019 The UltiSoC Project. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Char type
+ */
+
+#ifndef _BOOTROM_CTYPE_H_
+#define _BOOTROM_CTYPE_H_
+
+
+static inline
+int isspace(int c)
+{
+	return (c == ' ' || c == '\t');
+}
+
+
+static inline
+int isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+
+static inline
+int isxdigit(int c)
+{
+	return (isdigit(c) || ( c >= 'A' && c <= 'F') || ( c >= 'a' && c <= 'f'));
+}
+
+
+static inline
+int isbdigit(int c)
+{
+	return (c == '0' || c == '1');
+}
+
+
+#endif /* _BOOTROM_CTYPE_H_ */
